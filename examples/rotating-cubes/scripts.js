9710a22c10033374e19fd640b1fea0af59355bf1
Arjun Raj <arjun@athousandnodes.com>
2013-11-22 14:25:27 +0530

    Refactoring
    Added Separate pages for demos.
    Moved demos to demos folder
    Using CDN files

---
+++ examples/rotating-cubes/scripts.js
@@ -0,0 +1,38 @@
+
+$(function(){
+
+
+
+	perspective = {
+		x : parseInt($('#playground1').css('-webkit-perspective-origin').split(' ')[0].replace('px','')),
+		y : parseInt($('#playground1').css('-webkit-perspective-origin').split(' ')[1].replace('px',''))
+	}
+	zoom = 10.0;
+	moveBox(10);
+
+	$(window).keydown(function(e){
+		switch(e.keyCode){
+			case 40:
+			zoom++;
+			break;
+			case 38:
+			zoom--;
+			break;
+			case 32:
+			$('.box').toggleClass('play')
+			break;
+
+		}
+		console.log(e.keyCode, zoom, perspective);
+		moveBox();
+
+	})
+
+
+})
+
+function moveBox(){
+	$('.info .depth').text("Depth: " + (10 - zoom))
+	$('#playground1').css('-webkit-perspective-origin', (perspective.x-zoom) + "px " + perspective.y + "px")
+	$('#playground2').css('-webkit-perspective-origin', (perspective.x+zoom) + "px " + perspective.y + "px")
+}
